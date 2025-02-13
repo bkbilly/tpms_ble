9c6eb7718b4ce79fc979bc97804cf6402b3e604d
bkbilly <bkbilly@hotmail.com>
2025-02-13 08:30:45 +0200

    Fix data issues with new sensor

--- custom_components/tpms_ble/tpms_parser/parser.py
+++ custom_components/tpms_ble/tpms_parser/parser.py
@@ -45,17 +45,18 @@ class TPMSBluetoothDeviceData(BluetoothData):
         self.set_device_manufacturer("TPMS")
 
         if "000027a5-0000-1000-8000-00805f9b34fb" in service_info.service_uuids:
-            comp_hex = re.findall("..", hex(company_id)[2:].zfill(4))[::-1]
-            mfr_data = "".join(comp_hex).encode() + mfr_data
-            self._process_tpms_2(address, local_name, mfr_data)
+            self._process_tpms_2(address, local_name, mfr_data, company_id)
         elif company_id == 256:
             self._process_tpms_1(address, local_name, mfr_data)
+        else:
+            _LOGGER.error("Can't find the correct data type")
 
     def _process_tpms_1(self, address: str, local_name: str, data: bytes) -> None:
         """Parser for TPMS sensors."""
         _LOGGER.debug("Parsing TPMS sensor: %s", data)
         msg_length = len(data)
         if msg_length != 16:
+            _LOGGER.error("Can't parse the data because the data length should be 16")
             return
         (
             pressure,
@@ -65,58 +66,31 @@ class TPMSBluetoothDeviceData(BluetoothData):
         ) = unpack("=iib?", data[6:16])
         pressure = pressure / 100000
         temperature = temperature / 100
+        self._update_sensors(pressure, battery, temperature, alarm)
 
-        name = f"TPMS {short_address(address)}"
-        self.set_device_type(name)
-        self.set_device_name(name)
-        self.set_title(name)
-
-        self.update_sensor(
-            key=str(TPMSSensor.PRESSURE),
-            native_unit_of_measurement=None,
-            native_value=pressure,
-            name="Pressure",
-        )
-        self.update_sensor(
-            key=str(TPMSSensor.TEMPERATURE),
-            native_unit_of_measurement=None,
-            native_value=temperature,
-            name="Temperature",
-        )
-        self.update_sensor(
-            key=str(TPMSSensor.BATTERY),
-            native_unit_of_measurement=None,
-            native_value=battery,
-            name="Battery",
-        )
-        self.update_binary_sensor(
-            key=str(TPMSBinarySensor.ALARM),
-            native_value=bool(alarm),
-            name="Alarm",
-        )
-        self.update_sensor(
-            key=str(TPMSSensor.TIMESTAMP),
-            native_unit_of_measurement=None,
-            native_value=datetime.now().astimezone(),
-            name="Last Update",
-        )
-
-    def _process_tpms_2(self, address: str, local_name: str, data: bytes) -> None:
+    def _process_tpms_2(self, address: str, local_name: str, data: bytes, company_id: int) -> None:
         """Parser for TPMS sensors."""
         _LOGGER.debug("Parsing TPMS2 sensor: %s", data)
-        msg_length = len(data)
-        if msg_length != 7:
+        comp_hex = re.findall("..", hex(company_id)[2:].zfill(4))[::-1]
+        comp_hex = "".join(comp_hex)
+        data_hex = data.hex()
+
+        msg_length = len(data_hex)
+        if msg_length != 5:
+            _LOGGER.error("Can't parse the data because the data length should be 5")
             return
-        voltage = int(data[2:4], 16) / 10
-        temperature = int(data[4:6], 16)
-        psi_pressure = int(data[6:10], 16) / 10
+        voltage = int(comp_hex[2:4], 16) / 10
+        temperature = int(data_hex[0:2], 16)
+        psi_pressure = int(data_hex[2:6], 16) / 10
 
         pressure = round(psi_pressure * 0.0689476, 3)
         min_voltage = 2.6
         max_voltage = 3.3
         battery = ((voltage - min_voltage) / (max_voltage - min_voltage)) * 100
         battery = int(round(max(0, min(100, battery)), 0))
+        self._update_sensors(pressure, battery, temperature, None)
 
+    def _update_sensors(pressure, battery, temperature, alarm):
         name = f"TPMS {short_address(address)}"
         self.set_device_type(name)
         self.set_device_name(name)
@@ -140,6 +114,12 @@ class TPMSBluetoothDeviceData(BluetoothData):
             native_value=battery,
             name="Battery",
         )
+        if alarm is not None:
+            self.update_binary_sensor(
+                key=str(TPMSBinarySensor.ALARM),
+                native_value=bool(alarm),
+                name="Alarm",
+            )
         self.update_sensor(
             key=str(TPMSSensor.TIMESTAMP),
             native_unit_of_measurement=None,
